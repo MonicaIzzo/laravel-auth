74aa6ab6fc8cc338440ab2dcf488b936775c5576
MonicaIzzo <info@monicaizzo.net>
2023-09-20 05:10:10 +0200

    add Route Post

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\Guest\HomeController as GuestHomeController;
 use App\Http\Controllers\Admin\HomeController as AdminHomeController;
+use App\Http\Controllers\Admin\PostController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
@@ -21,6 +22,17 @@ Route::get('/', [GuestHomeController::class, 'index'])->name('guest.home');
 // Tutte le route per la parte amministrativa
 Route::prefix(('/admin'))->name(('admin.'))->middleware(['auth'])->group(function () {
     Route::get('/', [AdminHomeController::class, 'index'])->name('home');
+
+    /*
+    Route::get('/posts', [PostController::class, 'index'])->name('posts.index');
+    Route::get('/posts/create', [PostController::class, 'create'])->name('posts.create');
+    Route::get('/posts/{post}', [PostController::class, 'show'])->name('posts.show');
+    Route::get('/posts/{post}/edit', [PostController::class, 'edit'])->name('posts.edit');
+    Route::put('//posts/{post}/update', [PostController::class, 'update'])->name('posts.update');
+    Route::post('//posts/store', [PostController::class, 'store'])->name('posts.store');
+    Route::delete('//posts/{post}', [PostController::class, 'destroy'])->name('posts.destroy'); */
+
+    Route::resource('posts', PostController::class);
 });
 
 
