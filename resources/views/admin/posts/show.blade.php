5c8a1671afc77f5995d788328ee568b8a5e8f237
MonicaIzzo <info@monicaizzo.net>
2023-09-21 06:47:45 +0200

    add POST vista

---
+++ resources/views/admin/posts/show.blade.php
@@ -0,0 +1,32 @@
+@extends('layouts.app')
+
+@section('title', 'Posts')
+
+@section('content')
+    <h1>{{ $post->title }}</h1>
+    <hr>
+    <div class="clearfix">
+        <img class="float-start me-2" src="{{ $post->image }}" alt="{{ $post->title }}">
+        <p>{{ $post->content }}</p>
+        <div>
+            <strong>Creato il: </strong>{{ $post->created_at }}<br>
+            <strong>Ultima modifica: </strong>{{ $post->updated_at }}
+        </div>
+    </div>
+    <hr>
+    <footer class="d-flex justify-content-between">
+        <div class="">
+            <a href="{{ route('admin.posts.index', $post) }}" class="btn  btn-secondary"><i class="fa-solid fa-arrow-left"></i>
+                Torna indietro</a>
+        </div>
+        <div class="d-flex justify-content-end">
+            <a href="{{ route('admin.posts.edit', $post) }}" class="btn btn-warning ms-2"><i class="fas fa-pencil"></i>
+                Modifica</a>
+            <form action="{{ route('admin.posts.destroy', $post) }}" method="POST" class="ms-2">
+                @method('DELETE')
+                @csrf
+                <button class="btn  btn-danger"><i class="fas fa-trash"></i> Elimina</button>
+            </form>
+        </div>
+    </footer>
+@endsection
